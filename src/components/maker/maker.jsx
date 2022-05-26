614aee7d03130a0c06b8e6ff9cdf4c80a1bbdf17
JJangGyu <pridom112@naver.com>
2022-05-27 03:50:49 +0900

    handle state change on editor

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -8,41 +8,44 @@ import Preview from '../preview/preview';
 
 const Maker = ({ authService }) => {
 
-    const [cards, setCards] = useState([
-        {
-            id: '1',
-            name: 'test1',
-            company: 'dream',
-            theme: 'dark',
-            title: 'Software Engineer',
-            email: 'test@test.com',
-            message: 'im so sleep',
-            fileName : 'test',
-            fileURL: null
+    // 배열이 아니라 Object로 데이터를 저장
+    // id가 key값으로 저장시킬것 --> 배열로 map을 쓰는경우 배열이 늘어날때마다 성능이 저하된다.
+    // Object.keys로 배열 만들고 arr[id] 이런식으로 검색하려고
+    const [cards, setCards] = useState({
+        1: {
+          id: '1',
+          name: 'Ellie',
+          company: 'Samsung',
+          theme: 'dark',
+          title: 'Software Engineer',
+          email: 'ellie@gmail.com',
+          message: 'go for it',
+          fileName: 'ellie',
+          fileURL: null,
         },
-        {
-            id: '2',
-            name: 'test2',
-            company: 'dream',
-            theme: 'light',
-            title: 'Software Engineer',
-            email: 'test@test.com',
-            message: 'im so sleep',
-            fileName : 'test',
-            fileURL: 'test.png'
+        2: {
+          id: '2',
+          name: 'Ellie2',
+          company: 'Samsung',
+          theme: 'light',
+          title: 'Software Engineer',
+          email: 'ellie@gmail.com',
+          message: 'go for it',
+          fileName: 'ellie',
+          fileURL: 'ellie.png',
         },
-        {
-            id: '3',
-            name: 'test3',
-            company: 'dream',
-            theme: 'colorful',
-            title: 'Software Engineer',
-            email: 'test@test.com',
-            message: 'im so sleep',
-            fileName : 'test',
-            fileURL: null
-        }
-    ]);
+        3: {
+          id: '3',
+          name: 'Ellie3',
+          company: 'Samsung',
+          theme: 'colorful',
+          title: 'Software Engineer',
+          email: 'ellie@gmail.com',
+          message: 'go for it',
+          fileName: 'ellie',
+          fileURL: null,
+        },
+    });
 
     const navigate = useNavigate();
     const onLogout = () => {
@@ -58,17 +61,27 @@ const Maker = ({ authService }) => {
         });
     })
 
-    // Maker에서 Card 신규추가
-    const addCard = (card) => {
-        const updated = [...cards , card];
-        setCards(updated);
+    const createOrUpdateCard = card => {
+        setCards(cards => {
+            const updated = { ...cards };
+            updated[card.id] = card;
+            return updated;
+        });
+    };
+    
+    const deleteCard = card => {
+        setCards(cards => {
+            const updated = { ...cards };
+            delete updated[card.id];
+            return updated;
+        });
     };
 
     return (
         <section className={styles.maker}>
             <Header onLogout={onLogout}/>
             <div className={styles.container}>
-                <Editor cards={cards} addCard={addCard}/>
+                <Editor cards={cards} addCard={createOrUpdateCard} updateCard={createOrUpdateCard} deleteCard={deleteCard}/>
                 <Preview cards={cards}/>
             </div>
             <Footer />
